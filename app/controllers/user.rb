fa5d321c4dd3f5a3dc6844ae9948a09d0a80412c
Adedotun Oni  & Sam Glickman <pair+onidotun123.sglickman17@devbootcamp.com>
2017-02-02 12:02:00 -0500

    finished user controller and views

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,7 +2,7 @@ get '/user/new' do
   erb :'user/new' #show new user view
 end
 
-post '/user' do
+post '/user' do #new user post request
   @user = User.new(params[:user])
   if @user.save #saves new user or returns false if unsuccessful
     session[:user_id] = @user.id
@@ -11,4 +11,33 @@ post '/user' do
     @errors = @user.errors.full_messages
     erb :'user/new' # show new user view again(potentially displaying errors)
   end
+end
+
+get '/user/login' do #user login route
+  erb :'user/login'
+end
+
+post '/user/login' do #user login post request
+  @user = User.find_by(username: params[:user][:username])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = ["Username and password did not match"]
+    erb :'user/login'
+  end
+end
+
+get '/user/logout' do
+  session.clear
+  redirect '/'
+end
+
+get '/user/:id' do
+  @user = User.find(params[:id]) #define instance variable for view
+  erb :'user/profile' #show single user view
+end
+
+get '/user/profile' do
+  erb :'user/profile'
 end
