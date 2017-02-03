6a0446a56c17dea062a6cab7061bc25fd4364644
Josica Ganesamurthy <email": null>
2017-02-03 12:28:02 -0500

    add initial code for submit answer button

---
+++ app/controllers/answer.rb
@@ -0,0 +1,13 @@
+get '/answers/new' do
+  erb :'/answers/new'
+end
+
+post '/answers' do
+  # @user = current_user
+  # @answer = Answer.new(params[:answer])
+  # @answer.user = @user
+
+  # if @question.save
+    # redirect '/questions'
+  # else
+end
